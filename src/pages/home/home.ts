3d3ac38462360ceadee23a5f264e77761555a0c3
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2018-04-08 23:23:44 +0400

    styling

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Note} from "../../models/domain/note.interface";
+import {Tag} from "../../models/domain/tag.interface";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  notes: Note[]
+  tags: Tag[]
 
+  constructor(public navCtrl: NavController) {
+    this.notes = [
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'red'}, {id: 'sdfsf', name:'sdasdasd', color: 'red'},]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+      {id: 'dsfsdf', text: 'askdka', title: 'sdasdas', createDate: 'sdfdsfds', tags: [{id: 'sdfsf', name:'sdasdasd', color: 'black'}]},
+    ];
+
+    this.tags = [
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+      {id: 'asfasf', color: 'red', name: 'foo'},
+    ];
   }
 
 }
