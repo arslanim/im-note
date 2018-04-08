c1c3405f0daed0c45fbfcf8357491945312eb970
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2018-04-09 00:18:59 +0400

    changes page order

--- src/settings/pages.settings.ts
+++ src/settings/pages.settings.ts
@@ -3,7 +3,7 @@ import {HomePage} from "../pages/home/home";
 import {ListPage} from "../pages/list/list";
 
 export const PAGE_SETTINGS = [
-  {title: 'Notes', component: NotesPage, isRoot: false},
   {title: 'Home', component: HomePage, isRoot: true},
+  {title: 'Notes', component: NotesPage, isRoot: false},
   {title: 'List', component: ListPage, isRoot: false},
 ]
